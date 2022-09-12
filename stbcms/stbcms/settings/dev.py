dc0b1d620f41608e239c46331fb191d4ddb0fe26
jacoacoacob <jacoacoacob@protonmail.com>
2022-09-12 14:33:28 -0500

    first commit

---
+++ stbcms/stbcms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(2a8=eq$492$15x-!ea3n*s4=ru8o7m(47h02=8r$mp&4j(x8u"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
