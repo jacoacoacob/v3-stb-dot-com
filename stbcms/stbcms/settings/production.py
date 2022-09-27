affd52b6e0da069bc1423cc1b6d427677ec7f7e5
jacoacoacob <jacoacoacob@protonmail.com>
2022-09-26 22:36:49 -0500

    setup app to build and run in docker container

--- stbcms/stbcms/settings/production.py
+++ stbcms/stbcms/settings/production.py
@@ -1,9 +1,26 @@
-from stbcms.stbcms.settings.dev import ALLOWED_HOSTS
+import os
+
+import dj_database_url
+
+from stbcms.settings.dev import ALLOWED_HOSTS
 from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+DATABASES = {
+    'default': {
+        **dj_database_url.config(),
+        'CONN_MAX_AGE': 10,
+        'CONN_HEALTH_CHECKS': True,
+    }
+}
+
 
 try:
     from .local import *
