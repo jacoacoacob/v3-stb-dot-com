fe1057fa46d9305e1be1ec8e92a11e1eeb9c32ae
jacoacoacob <114765980+jacoacoacob@users.noreply.github.com>
2022-09-30 20:21:29 -0500

    Develop (#1)
    * force appending slash to configure-prod script REMOTE in rsync commands
    
    * restrict build-and-push workflow with paths

--- configure-prod.sh
+++ configure-prod.sh
@@ -1,18 +1,24 @@
 REMOTE=$1
 # REMOTE_LOCATION=$2
 
+if [[ ! $REMOTE ]]; then
+  echo "You must provide a remote. Use format <user>@<host>:</path/to/project/>"
+  exit 1
+fi
+
 # # Remove leading and tailing slashs, if either exist, from the user-provided 
 # # REMOTE_LOCATION so that we can ensure rsync works as expected
 # REMOTE_LOCATION=$(echo $REMOTE_LOCATION | sed 's#/$##' | sed 's#^/##')
+REMOTE=$(echo $REMOTE | sed 's#/$##')
 
 echo "Copying docker-compose.yml"
 # rsync -chv --progress docker-compose.yml $REMOTE:/$REMOTE_LOCATION/
-rsync -chv --progress docker-compose.yml $REMOTE
+rsync -chv --progress docker-compose.yml $REMOTE/
 
 echo "Copying Nginx config"
-rsync -achv --progress ./nginx-conf $REMOTE
+rsync -achv --progress ./nginx-conf $REMOTE/
 
 echo "Copying .env.prod"
-rsync -chv --progress .env.prod $REMOTE
+rsync -chv --progress .env.prod $REMOTE/
 
 echo "Done!"
