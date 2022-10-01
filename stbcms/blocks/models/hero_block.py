a16ab34f7f61af1a3c548dc60947d91b36467f8c
jacoacoacob <jacoacoacob@protonmail.com>
2022-10-01 17:53:45 -0500

    removed image field from home_page hero block and made cta link optional with listblock

--- stbcms/blocks/models/hero_block.py
+++ stbcms/blocks/models/hero_block.py
@@ -1,15 +1,15 @@
-from wagtail.blocks import StructBlock, RichTextBlock, CharBlock
+from wagtail.blocks import ListBlock, StructBlock, RichTextBlock, CharBlock
 from wagtail.images.blocks import ImageChooserBlock
 
 from blocks.models.link_block import LinkBlock
 
 class HeroBlock(StructBlock):
-  image = ImageChooserBlock()
+  # image = ImageChooserBlock(required=False)
   cta = StructBlock(
     [
       ("heading", CharBlock(max_length=140)),
       ("text", RichTextBlock(features=["h2", "h3", "h4", "bold", "italic", "ol", "ul", "link"])),
-      ("link", LinkBlock())
+      ("links", ListBlock(LinkBlock(), min_num=0, max_num=1))
     ],
     label="Call To Action"
   )
