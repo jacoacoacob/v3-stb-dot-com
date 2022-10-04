5bc6e31ac0e1768170cf62e372f9b35eef3638c4
jacoacoacob <114765980+jacoacoacob@users.noreply.github.com>
2022-10-04 11:56:36 -0500

    Develop (#3)
    * put wagtail_styleguide in dev only
    
    * added signal handlers to purge blog post listing page on child page update and applied latent migration
    
    * added cache purge signal handlers for event listing page on event page update
    
    * updated build and push workflow to only run on push

--- stbcms/events/models/event_listing_page.py
+++ stbcms/events/models/event_listing_page.py
@@ -6,9 +6,12 @@ class EventListingPage(Page):
   parent_page_types = ["home.HomePage"]
   subpage_types = ["events.EventPage"]
   
+  def get_events(self):
+    return EventPage.objects.child_of(self).live()
+
   def get_context(self, request, *args, **kwargs):
     context = super().get_context(request, *args, **kwargs)
-    events = EventPage.objects.child_of(self).live()
+    events = self.get_events()
     context["upcoming_events"] = [event for event in events if event.countdown_status != "is_over"]
     context["has_upcoming_events"] = len(context["upcoming_events"]) > 0
     context["past_events"] = [event for event in events if event.countdown_status == "is_over"]
