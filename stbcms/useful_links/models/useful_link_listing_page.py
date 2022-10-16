77677a68f6d5fe55336ad9614ade747b72025a5f
jacoacoacob <jacoacoacob@protonmail.com>
2022-10-16 12:53:21 -0500

    fixed AttributeError

--- stbcms/useful_links/models/useful_link_listing_page.py
+++ stbcms/useful_links/models/useful_link_listing_page.py
@@ -7,7 +7,10 @@ class UsefulLinkListingPage(Page):
   parent_page_types = ["home.HomePage"]
   subpage_types = ["useful_links.UsefulLinkPage"]
 
+  def get_useful_links(self):
+    return UsefulLinkPage.objects.child_of(self).live()
+
   def get_context(self, request, *args, **kwargs):
     context = super().get_context(request, *args, **kwargs)
-    context["links"] = UsefulLinkPage.objects.child_of(self).live()
+    context["links"] = self.get_useful_links()
     return context
