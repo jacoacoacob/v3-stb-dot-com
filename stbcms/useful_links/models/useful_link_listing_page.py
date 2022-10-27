4197734eb3dd8d9b69dd250a58b091a352bfa183
jacoacoacob <jacoacoacob@protonmail.com>
2022-10-27 16:32:34 -0500

    added usefullinklistingpage header text to model and template

--- stbcms/useful_links/models/useful_link_listing_page.py
+++ stbcms/useful_links/models/useful_link_listing_page.py
@@ -1,9 +1,30 @@
+from email.policy import default
+from django.db import models
+
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.models import Page
 
 from .useful_link_page import UsefulLinkPage
 from .useful_link_category import UsefulLinkCategories, UsefulLinkCategory
 
 class UsefulLinkListingPage(Page):
+  header_text = models.TextField(max_length=500)
+  disable_tags = models.BooleanField(
+    default=False,
+    help_text="Check this box to hide UI elements that show useful link Categories or Topics"
+  )
+
+  content_panels = Page.content_panels + [
+    FieldPanel("header_text"),
+    MultiFieldPanel(
+      [
+        FieldPanel("disable_tags"),
+      ],
+      heading="Feature Flags"
+    )
+  ]
+  
+
   parent_page_types = ["home.HomePage"]
   subpage_types = ["useful_links.UsefulLinkPage"]
 
